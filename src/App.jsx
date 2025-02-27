94ab75b8269b5d62d55b365f0bdb3fd288a8bad9
Veerasiva <veerasiva01@gmail.com>
2025-02-27 20:02:12 +0530

    feat: add preloader text as svg and animate the text with animejs

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,36 @@
-import { BrowserRouter } from 'react-router-dom'
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
+import React, { useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas, Preloader } from './components';
 
 const App = () => {
+   const [isPreloaderVisible, setPreloaderVisible] = useState(true);
+
+   const handlePreloaderComplete = () => {
+      setPreloaderVisible(false);
+   };
+
    return (
       <BrowserRouter>
-         <div className='relative z-0 bg-primary'>
-
-            <div className='bg-[url(/src/assets/herobg.png)] bg-cover bg-no-repeat bg-center'>
-               <Navbar />
-               <Hero />
+         {isPreloaderVisible && <Preloader onAnimationComplete={handlePreloaderComplete} />}
+         {!isPreloaderVisible && (
+            <div className="relative z-0 bg-primary">
+               <div className="bg-[url(/src/assets/herobg.png)] bg-cover bg-no-repeat bg-center">
+                  <Navbar />
+                  <Hero />
+               </div>
+               <About />
+               <Experience />
+               <Tech />
+               <Works />
+               <Feedbacks />
+               <div className="relative z-0">
+                  <Contact />
+                  <StarsCanvas />
+               </div>
             </div>
-
-            <About />
-            <Experience />
-            <Tech />
-            <Works />
-            <Feedbacks />
-
-            <div className='relative z-0'>
-               <Contact />
-               <StarsCanvas />
-
-            </div>
-
-         </div>
+         )}
       </BrowserRouter>
-   )
-}
+   );
+};
 
-export default App
+export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -7,10 +7,9 @@ import Experience from './Experience';
 import Works from './Works';
 import Feedbacks from './Feedbacks';
 import Contact from './Contact';
-import PreLoader from './PreLoader';
-
+import Preloader from './PreLoader';
 export {
-  PreLoader,
+  Preloader,
   Hero,
   Navbar,
   About,
