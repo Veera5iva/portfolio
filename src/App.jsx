656d80720846a25f0a3bb3bae83235e889f7841c
Veerasiva <veerasiva01@gmail.com>
2025-02-27 22:23:53 +0530

    fix: preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,31 @@
-import React, { useState } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas, Preloader } from './components';
+import { BrowserRouter } from 'react-router-dom'
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
 
 const App = () => {
-   const [isPreloaderVisible, setPreloaderVisible] = useState(true);
-
-   const handlePreloaderComplete = () => {
-      setPreloaderVisible(false);
-   };
-
    return (
       <BrowserRouter>
-         {isPreloaderVisible && <Preloader onAnimationComplete={handlePreloaderComplete} />}
-         {!isPreloaderVisible && (
-            <div className="relative z-0 bg-primary">
-               <div className="bg-[url(/src/assets/herobg.png)] bg-cover bg-no-repeat bg-center">
-                  <Navbar />
-                  <Hero />
-               </div>
-               <About />
-               <Experience />
-               <Tech />
-               <Works />
-               <Feedbacks />
-               <div className="relative z-0">
-                  <Contact />
-                  <StarsCanvas />
-               </div>
-            </div>
-         )}
-      </BrowserRouter>
-   );
-};
+         <div className='relative z-10000 bg-primary'>
 
-export default App;
+            <div className='bg-[url(/src/assets/herobg.png)] bg-cover bg-no-repeat bg-center'>
+               <Navbar />
+               <Hero />
+            </div>
+
+            <About />
+            <Experience />
+            <Tech />
+            <Works />
+            <Feedbacks />
+
+            <div className='relative z-0'>
+               <Contact />
+               <StarsCanvas />
+
+            </div>
+
+         </div>
+      </BrowserRouter>
+   )
+}
+
+export default App
