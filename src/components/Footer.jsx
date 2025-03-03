3acc968c052c83ce6ffdd45975f4e5a2df359be3
Veerasiva <veerasiva01@gmail.com>
2025-03-03 22:41:16 +0530

    feat: add sticky footer in the bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,30 @@
 import { TextPressure } from "./ui"
 const Footer = () => {
    return (
-      <div className="">
-         <TextPressure
-            text="Veerasiva!"
-            flex={true}
-            alpha={false}
-            stroke={true}
-            width={true}
-            weight={true}
-            italic={true}
-            textColor="#ffffff"
-            strokeColor="#ff0000"
-            minFontSize={36}
-            letterSpacing="0.05em"
-            textHeight={0.9}
-            hoverColor="#e23720"
-            hoverScale={1.15}
-         />
+      <div
+         className='relative h-[270px]'
+         style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
+      >
+         <div className='relative h-[calc(100vh+270px)] -top-[100vh]'>
+            <div className='h-[270px] sticky top-[calc(100vh-270px)]'>
+               <TextPressure
+                  text="Veerasiva!"
+                  flex={true}
+                  alpha={false}
+                  stroke={true}
+                  width={true}
+                  weight={true}
+                  italic={true}
+                  textColor="#ffffff"
+                  strokeColor="#ff0000"
+                  minFontSize={36}
+                  letterSpacing="0.05em"
+                  textHeight={0.9}
+                  hoverColor="#e23720"
+                  hoverScale={1.15}
+               />
+            </div>
+         </div>
       </div>
    )
 }
