450ec5c1b62756d4b635bfae56fa259c1f71406a
Veerasiva <veerasiva01@gmail.com>
2025-03-04 14:38:56 +0530

    fix: remove unwanted assets and imports

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,4 @@
 import {
-   mobile,
-   backend,
-   creator,
-   web,
    javascript,
    typescript,
    html,
@@ -16,10 +12,6 @@ import {
    git,
    figma,
    docker,
-   meta,
-   starbucks,
-   tesla,
-   shopify,
    carrent,
    jobit,
    tripguide,
@@ -41,24 +33,6 @@ export const navLinks = [
    },
 ];
 
-const services = [
-   {
-      title: "Full-Stack Developer",
-      icon: web,
-   },
-   {
-      title: "React.js & Next.js Developer",
-      icon: creator,
-   },
-   {
-      title: "Backend Developer",
-      icon: backend,
-   },
-   {
-      title: "Software Engineer",
-      icon: mobile,
-   },
-];
 
 const technologies = [
    {
@@ -121,87 +95,7 @@ const technologies = [
 
 const about = "I’m a Full-Stack Developer passionate about building scalable, user-friendly apps that solve real-world problems. I love clean code and turning ideas into impactful digital experiences. Let’s create something amazing together!";
 
-const experiences = [
-   {
-      title: "React.js Developer",
-      company_name: "Starbucks",
-      icon: starbucks,
-      iconBg: "#383E56",
-      date: "March 2020 - April 2021",
-      points: [
-         "Developing and maintaining web applications using React.js and other related technologies.",
-         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-         "Implementing responsive design and ensuring cross-browser compatibility.",
-         "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-   },
-   {
-      title: "React Native Developer",
-      company_name: "Tesla",
-      icon: tesla,
-      iconBg: "#E6DEDD",
-      date: "Jan 2021 - Feb 2022",
-      points: [
-         "Developing and maintaining web applications using React.js and other related technologies.",
-         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-         "Implementing responsive design and ensuring cross-browser compatibility.",
-         "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-   },
-   {
-      title: "Web Developer",
-      company_name: "Shopify",
-      icon: shopify,
-      iconBg: "#383E56",
-      date: "Jan 2022 - Jan 2023",
-      points: [
-         "Developing and maintaining web applications using React.js and other related technologies.",
-         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-         "Implementing responsive design and ensuring cross-browser compatibility.",
-         "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-   },
-   {
-      title: "Full stack Developer",
-      company_name: "Meta",
-      icon: meta,
-      iconBg: "#E6DEDD",
-      date: "Jan 2023 - Present",
-      points: [
-         "Developing and maintaining web applications using React.js and other related technologies.",
-         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-         "Implementing responsive design and ensuring cross-browser compatibility.",
-         "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-   },
-];
 
-const testimonials = [
-   {
-      testimonial:
-         "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-      name: "Sara Lee",
-      designation: "CFO",
-      company: "Acme Co",
-      image: "https://randomuser.me/api/portraits/women/4.jpg",
-   },
-   {
-      testimonial:
-         "I've never met a web developer who truly cares about their clients' success like Rick does.",
-      name: "Chris Brown",
-      designation: "COO",
-      company: "DEF Corp",
-      image: "https://randomuser.me/api/portraits/men/5.jpg",
-   },
-   {
-      testimonial:
-         "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-      name: "Lisa Wang",
-      designation: "CTO",
-      company: "456 Enterprises",
-      image: "https://randomuser.me/api/portraits/women/6.jpg",
-   },
-];
 
 const projects = [
    {
@@ -269,4 +163,4 @@ const projects = [
    },
 ];
 
-export { services, technologies, experiences, testimonials, projects, about };
+export { technologies, projects, about };
