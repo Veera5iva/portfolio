04dd6f96f4b57666ff032958b0d56e8554b7b950
Veerasiva <veerasiva01@gmail.com>
2025-03-05 10:23:08 +0530

    fix: change breakpoints and update the styles file accordingly

--- src/styles.js
+++ src/styles.js
@@ -1,7 +1,7 @@
 const styles = {
    paddingX: "sm:!px-16 !px-6",
    paddingY: "sm:!py-16 !py-6",
-   padding: "sm:!px-16 !px-6 sm:!py-16 !py-10",
+   padding: "md:!px-16 sm:!px-8 xs:!px-6 !px-6 md:!py-16 sm:!py-8 xs:!py-7 !py-6",
 
    heroHeadText:
       "font-black text-white lg:text-[70px] sm:text-[50px] xs:text-[44px] text-[40px] lg:leading-[98px] mt-2",
@@ -9,9 +9,9 @@ const styles = {
       "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
 
    sectionHeadText:
-      "text-transparent font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
+      "text-transparent font-black md:text-[60px] sm:text-[33px] xs:text-[31px] text-[30px]",
    sectionSubText:
-      "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
+      "md:text-[18px] sm:text-[16px] xs:text-[15px] text-[14px] text-secondary uppercase tracking-wider",
 };
 
 export { styles };
