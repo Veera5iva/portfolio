d06480873386967c805ed29226313c3c8d821de0
Veerasiva <veerasiva01@gmail.com>
2025-03-06 20:26:42 +0530

    fix: change all icons of tech to svg

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,16 +3,16 @@ import github from "./github.png";
 import preloaderText from "./preloaderText.svg";
 import veeraMobileLogo from "./veeraMobileLogo.svg";
 
-import css from "./tech/css.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import nextjs from "./tech/nextjs.png";
-import reactjs from "./tech/reactjs.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
+import css from "./tech/css.svg";
+import git from "./tech/git.svg";
+import html from "./tech/html.svg";
+import javascript from "./tech/javascript.svg";
+import mongodb from "./tech/mongodb.svg";
+import nodejs from "./tech/nodejs.svg";
+import nextjs from "./tech/nextjs.svg";
+import reactjs from "./tech/reactjs.svg";
+import tailwind from "./tech/tailwind.svg";
+import typescript from "./tech/typescript.svg";
 import java from "./tech/java.svg"
 import expressjs from "./tech/expressjs.svg"
 import mysql from "./tech/mysql.svg"
